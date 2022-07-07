fb2b74e77751c970f91f8d828a2a613537509dd5
Erwin <erwin.paralesda@gmail.com>
2022-07-07 16:10:21 -0500

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGroceryStoreAPI.Models
+{
+    public class Product
+    {
+
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public string Category { get; set; }
+        public DateTime ExpiryDate { get; set; }   
+
+    }
+}
